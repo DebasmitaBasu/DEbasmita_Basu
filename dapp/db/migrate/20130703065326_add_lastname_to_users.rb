086b286a34f3796b6f464359a00b1074306b51ac
DebasmitaBasu <debasmita.rd.basu10@gmail.com>
2013-07-08 08:34:57 +0530

    Remove

--- dapp/db/migrate/20130703065326_add_lastname_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddLastnameToUsers < ActiveRecord::Migration
-  def change
-   add_column:users, :Lastname, :string
-   rename_column:users, :Username, :Firstname
-  end
-end
--- dapp/db/migrate/20130705064414_create_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreatePosts < ActiveRecord::Migration
-  def change
-    create_table :posts do |t|
-      t.string :Designation
-      t.string :username
-      t.timestamps
-    end
-  end
-end
